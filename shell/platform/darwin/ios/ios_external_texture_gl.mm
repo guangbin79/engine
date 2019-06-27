6acbbf776766470456c0117326e5749ad13f86bc
guangbin <guangbin79@gmail.com>
2019-06-27 18:27:27 +0800

    实现iOS下纹理的软渲染

--- shell/platform/darwin/ios/ios_external_texture_gl.mm
+++ shell/platform/darwin/ios/ios_external_texture_gl.mm
@@ -12,6 +12,65 @@
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/gpu/GrBackendSurface.h"
 
+NSString * CVPixelFormatName(OSType type) {
+switch (type) {
+    case kCVPixelFormatType_1Monochrome:                   return @"kCVPixelFormatType_1Monochrome";
+    case kCVPixelFormatType_2Indexed:                      return @"kCVPixelFormatType_2Indexed";
+    case kCVPixelFormatType_4Indexed:                      return @"kCVPixelFormatType_4Indexed";
+    case kCVPixelFormatType_8Indexed:                      return @"kCVPixelFormatType_8Indexed";
+    case kCVPixelFormatType_1IndexedGray_WhiteIsZero:      return @"kCVPixelFormatType_1IndexedGray_WhiteIsZero";
+    case kCVPixelFormatType_2IndexedGray_WhiteIsZero:      return @"kCVPixelFormatType_2IndexedGray_WhiteIsZero";
+    case kCVPixelFormatType_4IndexedGray_WhiteIsZero:      return @"kCVPixelFormatType_4IndexedGray_WhiteIsZero";
+    case kCVPixelFormatType_8IndexedGray_WhiteIsZero:      return @"kCVPixelFormatType_8IndexedGray_WhiteIsZero";
+    case kCVPixelFormatType_16BE555:                       return @"kCVPixelFormatType_16BE555";
+    case kCVPixelFormatType_16LE555:                       return @"kCVPixelFormatType_16LE555";
+    case kCVPixelFormatType_16LE5551:                      return @"kCVPixelFormatType_16LE5551";
+    case kCVPixelFormatType_16BE565:                       return @"kCVPixelFormatType_16BE565";
+    case kCVPixelFormatType_16LE565:                       return @"kCVPixelFormatType_16LE565";
+    case kCVPixelFormatType_24RGB:                         return @"kCVPixelFormatType_24RGB";
+    case kCVPixelFormatType_24BGR:                         return @"kCVPixelFormatType_24BGR";
+    case kCVPixelFormatType_32ARGB:                        return @"kCVPixelFormatType_32ARGB";
+    case kCVPixelFormatType_32BGRA:                        return @"kCVPixelFormatType_32BGRA";
+    case kCVPixelFormatType_32ABGR:                        return @"kCVPixelFormatType_32ABGR";
+    case kCVPixelFormatType_32RGBA:                        return @"kCVPixelFormatType_32RGBA";
+    case kCVPixelFormatType_64ARGB:                        return @"kCVPixelFormatType_64ARGB";
+    case kCVPixelFormatType_48RGB:                         return @"kCVPixelFormatType_48RGB";
+    case kCVPixelFormatType_32AlphaGray:                   return @"kCVPixelFormatType_32AlphaGray";
+    case kCVPixelFormatType_16Gray:                        return @"kCVPixelFormatType_16Gray";
+    case kCVPixelFormatType_30RGB:                         return @"kCVPixelFormatType_30RGB";
+    case kCVPixelFormatType_422YpCbCr8:                    return @"kCVPixelFormatType_422YpCbCr8";
+    case kCVPixelFormatType_4444YpCbCrA8:                  return @"kCVPixelFormatType_4444YpCbCrA8";
+    case kCVPixelFormatType_4444YpCbCrA8R:                 return @"kCVPixelFormatType_4444YpCbCrA8R";
+    case kCVPixelFormatType_4444AYpCbCr8:                  return @"kCVPixelFormatType_4444AYpCbCr8";
+    case kCVPixelFormatType_4444AYpCbCr16:                 return @"kCVPixelFormatType_4444AYpCbCr16";
+    case kCVPixelFormatType_444YpCbCr8:                    return @"kCVPixelFormatType_444YpCbCr8";
+    case kCVPixelFormatType_422YpCbCr16:                   return @"kCVPixelFormatType_422YpCbCr16";
+    case kCVPixelFormatType_422YpCbCr10:                   return @"kCVPixelFormatType_422YpCbCr10";
+    case kCVPixelFormatType_444YpCbCr10:                   return @"kCVPixelFormatType_444YpCbCr10";
+    case kCVPixelFormatType_420YpCbCr8Planar:              return @"kCVPixelFormatType_420YpCbCr8Planar";
+    case kCVPixelFormatType_420YpCbCr8PlanarFullRange:     return @"kCVPixelFormatType_420YpCbCr8PlanarFullRange";
+    case kCVPixelFormatType_422YpCbCr_4A_8BiPlanar:        return @"kCVPixelFormatType_422YpCbCr_4A_8BiPlanar";
+    case kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange:  return @"kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange";
+    case kCVPixelFormatType_420YpCbCr8BiPlanarFullRange:   return @"kCVPixelFormatType_420YpCbCr8BiPlanarFullRange";
+    case kCVPixelFormatType_422YpCbCr8_yuvs:               return @"kCVPixelFormatType_422YpCbCr8_yuvs";
+    case kCVPixelFormatType_422YpCbCr8FullRange:           return @"kCVPixelFormatType_422YpCbCr8FullRange";
+    case kCVPixelFormatType_OneComponent8:                 return @"kCVPixelFormatType_OneComponent8";
+    case kCVPixelFormatType_TwoComponent8:                 return @"kCVPixelFormatType_TwoComponent8";
+    case kCVPixelFormatType_30RGBLEPackedWideGamut:        return @"kCVPixelFormatType_30RGBLEPackedWideGamut";
+    case kCVPixelFormatType_OneComponent16Half:            return @"kCVPixelFormatType_OneComponent16Half";
+    case kCVPixelFormatType_OneComponent32Float:           return @"kCVPixelFormatType_OneComponent32Float";
+    case kCVPixelFormatType_TwoComponent16Half:            return @"kCVPixelFormatType_TwoComponent16Half";
+    case kCVPixelFormatType_TwoComponent32Float:           return @"kCVPixelFormatType_TwoComponent32Float";
+    case kCVPixelFormatType_64RGBAHalf:                    return @"kCVPixelFormatType_64RGBAHalf";
+    case kCVPixelFormatType_128RGBAFloat:                  return @"kCVPixelFormatType_128RGBAFloat";
+    case kCVPixelFormatType_14Bayer_GRBG:                  return @"kCVPixelFormatType_14Bayer_GRBG";
+    case kCVPixelFormatType_14Bayer_RGGB:                  return @"kCVPixelFormatType_14Bayer_RGGB";
+    case kCVPixelFormatType_14Bayer_BGGR:                  return @"kCVPixelFormatType_14Bayer_BGGR";
+    case kCVPixelFormatType_14Bayer_GBRG:                  return @"kCVPixelFormatType_14Bayer_GBRG";
+    }
+    return @"UNKNOWN";
+}
+
 namespace flutter {
 
 IOSExternalTextureGL::IOSExternalTextureGL(int64_t textureId,
@@ -26,43 +85,17 @@ void IOSExternalTextureGL::Paint(SkCanvas& canvas,
                                  const SkRect& bounds,
                                  bool freeze,
                                  GrContext* context) {
-  if (!cache_ref_) {
-    CVOpenGLESTextureCacheRef cache;
-    CVReturn err = CVOpenGLESTextureCacheCreate(kCFAllocatorDefault, NULL,
-                                                [EAGLContext currentContext], NULL, &cache);
-    if (err == noErr) {
-      cache_ref_.Reset(cache);
-    } else {
-      FML_LOG(WARNING) << "Failed to create GLES texture cache: " << err;
-      return;
-    }
-  }
-  fml::CFRef<CVPixelBufferRef> bufferRef;
-  if (!freeze) {
-    bufferRef.Reset([external_texture_ copyPixelBuffer]);
-    if (bufferRef != nullptr) {
-      CVOpenGLESTextureRef texture;
-      CVReturn err = CVOpenGLESTextureCacheCreateTextureFromImage(
-          kCFAllocatorDefault, cache_ref_, bufferRef, nullptr, GL_TEXTURE_2D, GL_RGBA,
-          static_cast<int>(CVPixelBufferGetWidth(bufferRef)),
-          static_cast<int>(CVPixelBufferGetHeight(bufferRef)), GL_BGRA, GL_UNSIGNED_BYTE, 0,
-          &texture);
-      texture_ref_.Reset(texture);
-      if (err != noErr) {
-        FML_LOG(WARNING) << "Could not create texture from pixel buffer: " << err;
-        return;
-      }
-    }
-  }
-  if (!texture_ref_) {
-    return;
-  }
-  GrGLTextureInfo textureInfo = {CVOpenGLESTextureGetTarget(texture_ref_),
-                                 CVOpenGLESTextureGetName(texture_ref_), GL_RGBA8_OES};
-  GrBackendTexture backendTexture(bounds.width(), bounds.height(), GrMipMapped::kNo, textureInfo);
-  sk_sp<SkImage> image =
-      SkImage::MakeFromTexture(context, backendTexture, kTopLeft_GrSurfaceOrigin,
-                               kRGBA_8888_SkColorType, kPremul_SkAlphaType, nullptr);
+  SkPixmap pixmap;
+  CVPixelBufferRef pixels = [external_texture_ copyPixelBuffer];
+
+  CVPixelBufferLockBaseAddress(pixels, kCVPixelBufferLock_ReadOnly);
+
+  pixmap.reset(SkImageInfo::Make(CVPixelBufferGetWidth(pixels), CVPixelBufferGetHeight(pixels), kBGRA_8888_SkColorType, kPremul_SkAlphaType),//kOpaque_SkAlphaType),
+                    CVPixelBufferGetBaseAddress(pixels), CVPixelBufferGetBytesPerRow(pixels));
+  sk_sp<SkImage> image = SkImage::MakeRasterCopy(pixmap);
+
+  CVPixelBufferUnlockBaseAddress(pixels, kCVPixelBufferLock_ReadOnly);
+
   FML_DCHECK(image) << "Failed to create SkImage from Texture.";
   if (image) {
     canvas.drawImage(image, bounds.x(), bounds.y());
