ebb02a7955aaf7a8a76ffa5d367bbf18f2cf2b0d
guangbin <guangbin79@gmail.com>
2019-07-24 15:22:50 +0800

    写入版本号

--- shell/platform/darwin/ios/framework/Source/FlutterEngine.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterEngine.mm
@@ -74,7 +74,7 @@
 - (instancetype)initWithName:(NSString*)labelPrefix
                      project:(FlutterDartProject*)projectOrNil
       allowHeadlessExecution:(BOOL)allowHeadlessExecution {
-  NSLog(@"FlutterEngine version: c1");
+  NSLog(@"WeLink-FlutterEngine: presentation-v1.7.8+hotfix.1");
   self = [super init];
   NSAssert(self, @"Super init cannot be nil");
   NSAssert(labelPrefix, @"labelPrefix is required");
